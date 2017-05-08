a57173a3dce222960cdde35a73386c194d930344
Kevin Murray <kdmfoss@gmail.com>
2017-05-08 11:44:26 +1000

    rename to pharephase

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-    name="parestahp",
+    name="parephase",
     install_requires=[
         "gffutils",
         "numpy",
         "pysam",
     ],
-    version="0.1.0",
-    scripts=["parestahp"],
+    version="0.2.0",
+    scripts=["parephase"],
     author="Kevin D Murray",
     author_email="kdmpapers@gmail.com",
     description="Detects phasing of 5'PARE data around stop codons",
     license="MPL2",
-    url="https://github.com/kdmurray91/parestahp",
+    url="https://github.com/kdmurray91/parephase",
     keywords="bioinformatics PARE RNA",
 )
